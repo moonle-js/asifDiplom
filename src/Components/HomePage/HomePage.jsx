908ee5530d8015d646c2cbf7c20ad453a2d2ded0
Abbasov <MABBA952@marriott.com>
2024-05-31 17:51:29 +0400

    Learn to add images

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,9 +1,30 @@
+import { useRef, useState } from "react"
+
 export default function HomePage(){
-    return(
-        <>
-            <h1>
-                Home Page
-            </h1>
-        </>
-    )
+    const [imageSrc, setImageSrc] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    // Display the image directly
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImageSrc(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageChange} />
+      {imageSrc && (
+        <div>
+          <h2>Selected Image:</h2>
+          <img src={imageSrc} alt="Selected" style={{ width: '300px' }} />
+        </div>
+      )}
+    </div>
+  );
 }
